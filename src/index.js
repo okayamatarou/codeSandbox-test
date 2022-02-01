17995d7b0df0d87ad52fa726b697c75f8efadf29
中原龍一 <88554232+okayamatarou@users.noreply.github.com>
2022-02-01 22:50:20 +0900

    20 アロー関数

--- src/index.js
+++ src/index.js
@@ -47,16 +47,36 @@
 // val5.push("monkey");
 // console.log(val5);
 
-/**
- * 19 テンプレート文字列
- */
-const name = "name";
-const age = 28;
+// /**
+//  * 19 テンプレート文字列
+//  */
+// const name = "name";
+// const age = 28;
 
-// 従来法
-const message1 = "私の名前は " + name + "です " + "年齢は" + age;
-console.log(message1);
+// // 従来法
+// const message1 = "私の名前は " + name + "です " + "年齢は" + age;
+// console.log(message1);
 
-// テンプレート文字列を用いる
-const message2 = `私の名前は${name}です。年齢は${age}です`;
-console.log(message2);
+// // テンプレート文字列を用いる
+// const message2 = `私の名前は${name}です。年齢は${age}です`;
+// console.log(message2);
+
+// 20 アロー関数
+
+// function func1(str) {
+//   return str;
+// }
+// const func1 = function(str){return str}
+// console.log(func1("オウム返し"));
+
+// アロー関数
+const func2 = (str) => str;
+
+console.log(func2("func2です"));
+
+// アロー関数　2変数
+const func3 = (num1, num2) => {
+  return num1 + num2;
+};
+
+console.log(func3(10, 20));
