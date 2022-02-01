38f5a639a6c4fdcc9afae7166ce6407bad5c1da0
中原龍一 <88554232+okayamatarou@users.noreply.github.com>
2022-02-01 22:39:35 +0900

    18 let and const

--- README.md
+++ README.md
@@ -1,6 +1,2 @@
 # codeSandbox-test
 Created with CodeSandbox
-
-Udemy ： 「モダンJavsScript の基礎から始める挫折しないためのReact入門」
-
-https://www.udemy.com/course/modern_javascipt_react_beginner/
--- src/index.js
+++ src/index.js
@@ -1,14 +1,47 @@
 /**
  * コメント
  */
-var val1 = "var変数だよ";
-console.log(val1);
+//var val1 = "var変数だよ";
+//console.log(val1);
 
 // var 変数は上書き可能
 
-val1 = "うわがきしちゃった";
-console.log(val1);
+//val1 = "うわがきしちゃった";
+//console.log(val1);
 
 // var変数は再宣言可能
-var val1 = "var変数を再宣言";
-console.log(val1);
+//var val1 = "var変数を再宣言";
+//console.log(val1);
+
+// let val2 = "let変数";
+// console.log(val2);
+
+// // let は上書きが可能
+// val2 = "let変数を上書き";
+// console.log(val2);
+
+// // letは再宣言不可能
+// let val2 = "let変数を再宣言"
+
+// const
+
+// const val3 = "const変数";
+// console.log(val3);
+
+// // val3 = "const変数の上書き"
+
+// // const で定義したオブジェクトはプロパティーの変更が可能
+// const val4 = {
+//   name: "じゃけえ",
+//   age: 28
+// };
+
+// val4.name = "job";
+// val4.addres = "Hiroshima";
+// console.log(val4);
+
+//　コメント化　Ctrl-K　Ctrl-C
+
+const val5 = ["dog", "cat"];
+val5[0] = "dog2";
+console.log(val5);
